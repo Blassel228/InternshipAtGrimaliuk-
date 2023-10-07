170e921a4413ab794cce71cf593fd3b6ceec3891
Андрій Тимофійчук <hok.ykrop@gmail.com>
2023-10-08 00:53:46 +0300

    Я додав docker-compose, вроді би правильно зробив асинхронні функції для коннесшена до редіса і постгреса, якимось чином я змусив працювати конфіг, redis я підняв не через ту бібліотеку бо якщо я правильно зрозумів у мене не підтримується aioredis.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
 from config import settings
+from databases import Database
+import asyncio_redis
+import redis
 
 app = FastAPI()
+
+async def redis_connect():
+    redis_conn = asyncio_redis.Connection.create(host=settings.redis_host, port=settings.redis_port)
+    #r = redis.Redis(host=settings.redis_host, port=settings.redis_port)
+    return redis_conn
+
+async def postgresql_connection():
+    postgres_conn = Database(f'postgresql://{settings.postgresql_user}:{settings.postgresql_password}'
+                             f'@{settings.postgresql_host}:{settings.postgresql_port}/{settings.postgresql_database_name}')
+    await postgres_conn.connect()
+    return postgres_conn
 
 @app.get("/")
 def read_root():
--- config.py
+++ config.py
@@ -1,9 +1,22 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import dotenv
 
 class Settings(BaseSettings):
-    host: str = "127.0.0.1"
-    port: int = 8000
-    model_config = SettingsConfigDict(env_file=".env")
+    host: str
+    port: int
+
+    redis_host: str
+    redis_port: int
+
+    postgresql_host: str
+    postgresql_port: int
+    postgresql_database_name: str
+    postgresql_password: str
+    postgresql_user: str
+
+
+    model_config = SettingsConfigDict(env_file=dotenv.find_dotenv(), extra = "allow")
+
 
 settings = Settings()
 
