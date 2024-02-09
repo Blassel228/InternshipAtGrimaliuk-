ee20c60ab86e2413a32a33d9fda19eb83e75ab3c
Андрій Тимофійчук <hok.ykrop@gmail.com>
2024-02-09 14:34:49 +0200

    8.1 Fixes deps.py was added to utils folder, get_current_user and get_db were inserted into there. Prefixes for routers were added, admon (!!!!!!!!) was changed to admin

---
+++ app/utils/deps.py
@@ -0,0 +1,34 @@
+from typing import Annotated
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+from app.db.models.models import UserModel, session
+from config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token/token/")
+async def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        await db.close()
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)],db: AsyncSession = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(token, settings.secret, algorithms=[settings.algorithm])
+        user_id: int = payload.get("id")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    res = await db.execute(select(UserModel).where(UserModel.id == user_id))
+    user = res.scalar()
+    if user is None:
+        raise credentials_exception
+    return user
