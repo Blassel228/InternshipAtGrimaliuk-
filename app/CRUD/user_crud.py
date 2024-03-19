da7e6419d3a3299483b4aed2e3d1552c60e02577
Андрій Тимофійчук <hok.ykrop@gmail.com>
2024-03-19 23:55:51 +0200

    10.3 self_update is relocated to user_service.py. Some files that are related to user logic were changed

--- app/CRUD/user_crud.py
+++ app/CRUD/user_crud.py
@@ -1,13 +1,9 @@
 from app.db.models.models import UserModel
-from passlib.context import CryptContext
 from sqlalchemy import update, select, insert
 from app.repositories.repository import CrudRepository
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.schemas.schemas import User
-from app.schemas.schemas import UserUpdate
-from fastapi import HTTPException
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from app.schemas.schemas import User, UserUpdate
+from app.core.autho import pwd_context
 
 class UserCrud(CrudRepository):
     async def update(self, id_: int, db: AsyncSession, data: User):
@@ -21,6 +17,16 @@ class UserCrud(CrudRepository):
         await db.commit()
         return res
 
+    async def self_update(self, id_: int, db: AsyncSession, data: dict):
+        stmt = update(self.model).values(**data).where(self.model.id == id_)
+        res = await db.execute(stmt)
+        if res.rowcount == 0:
+            return None
+        await db.commit()
+        stmt = select(self.model).where(self.model.id == id_)
+        res = await db.scalar(stmt)
+        return res
+
     async def add(self, db: AsyncSession, data: User):
         data = data.model_dump()
         stmt = insert(self.model).values(hashed_password=pwd_context.hash(data.pop("password")), **data)
@@ -30,19 +36,5 @@ class UserCrud(CrudRepository):
         await db.commit()
         res = await db.scalars(select(self.model).where(self.model.id == data["id"]))
         return res.first()
-
-    async def self_update(self, id_: int, db: AsyncSession, data: UserUpdate):
-        if data.password is not None:
-            data.password = pwd_context.hash(data.password)
-        data_dict = data.model_dump(exclude={"id", "update_by"}, exclude_none=True)
-        data_dict["hashed_password"] = data_dict.pop("password")
-        if not data_dict:
-            raise HTTPException(detail="Data is not full-filled", status_code=403)
-        stmt = (update(self.model).values(**data_dict).where(self.model.id == id_))
-        await db.execute(stmt)
-        await db.commit()
-        stmt = select(self.model).where(self.model.id == id_)
-        res = await db.scalar(stmt)
-        return res
 
 user_crud = UserCrud(UserModel)
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -10,18 +10,18 @@ user_router = APIRouter(tags=["user"], prefix="/user")
 
 @user_router.put("/self_update")
 async def self_update(data: UserUpdateIn, current_user = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
-    return await user_service.self_update(data=UserUpdate(**data.model_dump(), update_by=current_user.id), db=db)
+    return await user_service.self_update(id_=current_user.id,data=UserUpdate(**data.model_dump(), update_by=current_user.id), db=db)
 
 @user_router.delete("/self_delete")
 async def user_delete(current_user = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
-    return await user_crud.delete(id_=current_user.id, db=db)
+    return await user_service.delete(id_=current_user.id, db=db)
 @user_router.put("/")
 async def update(db: AsyncSession = Depends(get_db), data: User = None, id_: int = None):
-    return await user_crud.update(id_=id_, db=db, data=data)
+    return await user_service.update(id_=id_, db=db, data=data)
 
 @user_router.delete("/")
 async def delete(db: AsyncSession = Depends(get_db), id_: int = None):
-    return await user_crud.delete(id_=id_, db=db)
+    return await user_service.delete(id_=id_, db=db)
 
 @user_router.get("{id}")
 async def get(id_: int = None, db: AsyncSession = Depends(get_db)):
